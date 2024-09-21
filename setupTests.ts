ca0dd18017c637505a875aab9a645e67c58cf503
Ramandeep Bhagat <rdbhagat999@gmail.com>
2024-09-22 00:06:34 +0530

    updates setupTests.ts

--- setupTests.ts
+++ setupTests.ts
@@ -1,7 +1,11 @@
 import { afterAll, afterEach, beforeAll } from "vitest";
-import { cleanup } from "@testing-library/react";
+import { cleanup, configure } from "@testing-library/react";
 import "@testing-library/jest-som/vitest";
 import { server } from "./src/__mocks__/msw/node/server";
+
+configure({
+  asyncUtilTimeout: 5000,
+});
 
 beforeAll(() => server.listen());
 
