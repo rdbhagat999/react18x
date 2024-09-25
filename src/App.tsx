5e3f0296667663062c2cb25591e5a96a01d8524a
Ramandeep Bhagat <rdbhagat999@gmail.com>
2024-09-26 05:12:33 +0530

    useState hook example

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,65 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [sync, setSync] = useState(false);
+  const [name, setName] = useState(false);
+  const [username, setUsername] = useState({
+    firstName: "Ramandeep",
+    lastName: "Bhagat",
+  });
 
-  useEffect(() => {
-    document.title = "hello_" + count;
-  }, [sync, count]);
+  const toggleName = () => {
+    setName((v) => !v);
+  };
+
+  const updateUsername = (event: ChangeEvent<HTMLInputElement>) => {
+    // setUsername((prev) => ({ ...prev, firstName: "Ankit" }));
+    const fName = event?.target?.value;
+    setUsername((prev) => ({ ...prev, firstName: fName }));
+  };
+
+  const resetUsername = () => {
+    setUsername(() => ({ firstName: "Ramandeep", lastName: "Bhagat" }));
+  };
 
   return (
     <>
-      <h1 className="text-3xl text-red-500 font-bold underline">
-        Hello world!
-      </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <form className="flex flex-col gap-10">
+        <div>
+          <h1 className="text-3xl text-red-500 font-bold underline">
+            {name ? "useStateHook" : "Hello world!"}
+          </h1>
 
-        <button onClick={() => setSync((sync) => !sync)}>
-          sync is {sync ? "true" : "false"}
+          <button
+            type="button"
+            className="px-4 py-2 bg-blue-500 text-white rounded w-48 mx-auto my-2"
+            onClick={toggleName}
+          >
+            Toggle Name
+          </button>
+        </div>
+
+        <div>
+          <h2 className="text-3xl text-red-500 font-bold underline">
+            {username.firstName} {username.lastName}
+          </h2>
+
+          <input
+            type="text"
+            className="rounded p-2 mx-auto bg-slate-100 my-2 border"
+            value={username.firstName}
+            onChange={updateUsername}
+          />
+        </div>
+
+        <button
+          type="button"
+          className="px-4 py-2 bg-blue-500 text-white rounded w-48 mx-auto my-2"
+          onClick={resetUsername}
+        >
+          Reset Username
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </form>
     </>
   );
 }
