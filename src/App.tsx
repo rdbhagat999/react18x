944ca9b705c5db930e534b03c9e3bee9d7f31c2f
Ramandeep Bhagat <rdbhagat999@gmail.com>
2024-09-29 13:29:12 +0530

    adds useRefHook example

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,65 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const countRef = useRef(0);
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const [count, setCount] = useState(0);
-  const [sync, setSync] = useState(false);
+
+  const handleCount = () => {
+    inputRef?.current?.focus();
+    countRef.current++;
+    console.log("handleCount", countRef.current);
+  };
 
   useEffect(() => {
-    document.title = "hello_" + count;
-  }, [sync, count]);
+    console.log("[useEffect] [onmount] component rendered", countRef.current);
+  }, []);
+
+  useEffect(() => {
+    console.log("[useEffect] component rendered", countRef.current);
+  });
+
+  console.log("component rendered", countRef.current);
 
   return (
     <>
       <h1 className="text-3xl text-red-500 font-bold underline">
-        Hello world!
+        RefCount is {countRef.current}
       </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h2 className="text-3xl text-red-500 font-bold underline">
+        Count is {count}
+      </h2>
+      <div className="flex flex-col justify-center items-center gap-4">
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your email
+          </label>
+          <input
+            ref={inputRef}
+            type="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="name@email.com"
+            required
+          />
+        </div>
+        <button
+          className="px-4 py-2 rounded bg-blue-500 text-white"
+          onClick={handleCount}
+        >
+          + RefCount
         </button>
-
-        <button onClick={() => setSync((sync) => !sync)}>
-          sync is {sync ? "true" : "false"}
+        <button
+          className="px-4 py-2 rounded bg-blue-500 text-white"
+          onClick={() => setCount(count + 1)}
+        >
+          + Count
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
