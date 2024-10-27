bb80b448ee90b760954c686c90ba924a0eba9e53
Ramandeep Bhagat <rdbhagat999@gmail.com>
2024-10-27 15:15:53 +0530

    removes watch

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -11,7 +11,7 @@ const RegisterForm = () => {
   const {
     register,
     handleSubmit,
-    watch,
+    // watch,
     formState: { errors },
   } = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
